2037634a0f219ae6a5b284411f1fefe93ec756e3
Sanil14 <saniljalan14@gmail.com>
2023-12-17 01:44:17 +0000

    save settings in extensions storage

---
+++ uiScript.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  chrome.storage.sync.get(null, (items) => {
+    var keys = Object.keys(items);
+    if (keys.length != 4) {
+      // Not setup
+      $("input").each(function() {
+        var field = $(this).attr("id");
+        console.log(field);
+        items[field] = true;
+        chrome.storage.sync.set(items);
+      });
+    }
+    for (item in items) {
+      $(`input#${item}`).prop("checked", items[item]);
+    }
+  });
+});
+
+$("input").on("click", function () {
+  var field = $(this).attr("id");
+  var checked = $(this).prop("checked");
+
+  chrome.storage.sync.set({ [field]: checked });
+});
