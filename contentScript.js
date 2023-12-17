11cf605d936551f8f2fe64d5dbc0e82ab5e83905
Sanil14 <saniljalan14@gmail.com>
2023-12-17 02:31:43 +0000

    make font setting work + move fonts into folder

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,25 @@
 // alert("runs!");
 $(document).ready(function () {
   console.log("--- Disassist Extension Loaded ---");
-  var fontFace = `
-  @font-face {
-    font-family: 'font-regular';
-    src: url('${chrome.runtime.getURL("regular.woff")}') format('woff');
-  }
-`;
+  chrome.storage.sync.get(null, (items) => {
+    if (items?.font) {
+      var fontFace = `
+        @font-face {
+          font-family: 'font-regular';
+          src: url('${chrome.runtime.getURL("fonts/regular.woff")}') format('woff');
+        }
+      `;
 
-  // Append style to the head of the current document
-  $("head").append("<style>" + fontFace + "</style>");
-  $("body").attr(
-    "style",
-    "font-family: 'font-regular', sans-serif !important;"
-  );
+      // Append style to the head of the current document
+      $("head").append("<style>" + fontFace + "</style>");
+      $("body").attr(
+        "style",
+        "font-family: 'font-regular', sans-serif !important;"
+      );
+    }
+
+    chrome.storage.sync.onChanged.addListener(function (item) {
+      // Used to connect settings changes with page
+    });
+  });
 });
